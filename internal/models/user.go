8cbbd9c74d74373b0e41f9a9851196ff535d75f8
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2026-06-29 18:10:17 +0200

    task: add user model

---
+++ internal/models/user.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint   `gorm:"primary_key"`
+	Email     string `gorm:"uniqueIndex;not null"`
+	Password  string `gorm:"not null"`
+	FirstName string `gorm:";not null"`
+	LastName  string `gorm:";not null"`
+	Phone     string
+	IsActive  bool     `gorm:"default:true"`
+	Role      UserRole `gorm:"default:customer"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	//Relatiosnhip
+	RefreshTokens []RefreshToken
+	Orders        []Order
+	Cart          Cart
+}
+
+type UserRole string
+
+const (
+	UserRoleAdmin    UserRole = "admin"
+	UserRoleCustomer UserRole = "customer"
+)
+
+type RefreshToken struct {
+	ID        uint      `gorm:"primary_key"`
+	UserID    uint      `gorm:"not null"`
+	Token     string    `gorm:"uniqueIndex;not null"`
+	ExpiresAt time.Time `gorm:"not null"`
+	CreatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	User      User
+}
