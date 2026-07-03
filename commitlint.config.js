b0483e69929fff051119fcc0a5f29fe6580b6238
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2026-07-03 18:15:04 +0200

    task: update project setup

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+export default {
+    extends: ["@commitlint/config-conventional"],
+    rules:{
+        "type-enum": [2,"always",["ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "wip", "task"]]
+    }
+}
