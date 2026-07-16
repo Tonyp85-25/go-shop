e78f261556d6abbc3d50a26b5bf194546c369592
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2026-07-16 23:17:06 +0200

    task: fix migrations name

--- db/migrations/20260716152023_add_user.up.sql
+++ db/migrations/20260716152023_add_user.up.sql
@@ -1,16 +1,16 @@
 CREATE TYPE user_role AS ENUM ('customer', 'admin');
 
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    publicId UUID NOT NULL,
+    public_id UUID NOT NULL,
     email VARCHAR (255) UNIQUE NOT NULL,
     password VARCHAR (255) NOT NULL,
-    role VARCHAR (255) NOT NULL,
+    role user_role DEFAULT 'customer',
     is_active BOOLEAN DEFAULT true,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP WITH TIME ZONE
-)
+);
 
 CREATE INDEX idx_users_email ON users (email);
 
